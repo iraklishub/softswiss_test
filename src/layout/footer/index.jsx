6dedf46a78d548b9f4ceafe24696a66524e20118
iraklishub <51046973+iraklishub@users.noreply.github.com>
2024-07-27 21:52:29 +0400

    footer

--- src/layout/footer/index.jsx
+++ src/layout/footer/index.jsx
@@ -1,5 +1,12 @@
+import rocket from '../../assets/rocket.png'
+
 const Footer = () => {
-  return <footer className="footer">Footer</footer>
+  return (
+    <footer className="footer">
+      <img src={rocket} alt="rocket icon" width={52} height={78} />
+      <span>Exciting space adventure!</span>
+    </footer>
+  )
 }
 
 export default Footer
